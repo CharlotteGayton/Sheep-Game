d1084b1dd2e094647644b81904dc6339b42162f2
Charlotte <charlotte.gayton@gmail.com>
2023-01-25 19:34:20 +0000

    Updates

--- sheepgame.py
+++ sheepgame.py
@@ -4,39 +4,45 @@ def main():
 
     pygame.init()
 
-    logo, sheep = load_assets()
+    logo, sheep, background, bucket = load_assets()
 
     pygame.display.set_icon(logo)
     pygame.display.set_caption("Sheep Game")
 
     # Creating our surface
-    screen = pygame.display.set_mode((1000,600))
+    screen = pygame.display.set_mode((1280,720))
 
     #spawning sheep
 
     running = True
 
-    p1=10
-    p2=10
-    step=5
+    sheep_x=10
+    sheep_y=10
+
+    bucket_x=100
+    bucket_y=400
+
+    step=7
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
-        screen.blit(sheep, (p1,p2))
+        screen.blit(background, (0, 0))
+        screen.blit(bucket, (bucket_x, bucket_y))
+        screen.blit(sheep, (sheep_x,sheep_y))
+
 
         key_input = pygame.key.get_pressed()
 
         if key_input[pygame.K_LEFT]:
-            p1 -= step
+            sheep_x -= step
         if key_input[pygame.K_UP]:
-            p2 -= step
+            sheep_y -= step
         if key_input[pygame.K_RIGHT]:
-            p1 += step
+            sheep_x += step
         if key_input[pygame.K_DOWN]:
-            p2 += step
+            sheep_y += step
            
         pygame.display.flip() 
         pygame.display.update()    
@@ -51,8 +57,14 @@ def load_assets():
 
     #import sheep 
     sheep = pygame.image.load("Cotswold_Sheep.png")
+    sheep = pygame.transform.scale(sheep, (225,300))
 
-    return logo, sheep
+    background = pygame.image.load("gameBackground.png")
+
+    bucket = pygame.image.load("bucket-of-water.png")
+    bucket = pygame.transform.scale(bucket, (210,210))
+
+    return logo, sheep, background, bucket
 
 if __name__=="__main__":
     main()
