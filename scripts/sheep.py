fd24dfe0f11218223060d38d2e2feac007ec7f30
Charlotte <charlotte.gayton@gmail.com>
2023-02-06 22:09:29 +0000

    added new sheep images

--- scripts/sheep.py
+++ scripts/sheep.py
@@ -3,10 +3,11 @@ import pygame
 class SheepChar:
 
     def __init__(self):
-        self.height = 210
-        self.width = 168
-        self.image = pygame.image.load("assets/Cotswold_Sheep.png")
-        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.height = 150
+        self.widthHorizontal = 216
+        self.widthVertical = 111
+        self.image = pygame.image.load("assets/sheepFront.png")
+        self.image = pygame.transform.scale(self.image, (self.widthVertical, self.height))
         self.x_position = 10
         self.y_position = 10
         self.step = 7       
@@ -15,16 +16,24 @@ class SheepChar:
 
         if key_input[pygame.K_LEFT]:
             self.x_position -= self.step
+            self.image = pygame.image.load("assets/sheepLeft.png")
+            self.image = pygame.transform.scale(self.image, (self.widthHorizontal, self.height))
         if key_input[pygame.K_UP]:
             self.y_position -= self.step
+            self.image = pygame.image.load("assets/sheepBack.png")
+            self.image = pygame.transform.scale(self.image, (self.widthVertical, self.height))
         if key_input[pygame.K_RIGHT]:
             self.x_position += self.step
+            self.image = pygame.image.load("assets/sheepRight.png")
+            self.image = pygame.transform.scale(self.image, (self.widthHorizontal, self.height))
         if key_input[pygame.K_DOWN]:
             self.y_position += self.step
+            self.image = pygame.image.load("assets/sheepFront.png")
+            self.image = pygame.transform.scale(self.image, (self.widthVertical, self.height))
     
     def draw(self, screen):
         screen.blit(self.image, (self.x_position, self.y_position))
 
     @property
     def rect(self):
-        return pygame.Rect(self.x_position, self.y_position, self.width, self.height)
+        return pygame.Rect(self.x_position, self.y_position, self.widthVertical, self.height)
